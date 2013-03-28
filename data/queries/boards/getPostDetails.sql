d4a13936d9ef4f646ec80a138e323d419735c8d2
matthew rowe <mrowe@iMac.local>
2013-03-28 18:28:32 +0000

    Added lifetimes code for Boards.ie, SAP and Server Fault

---
+++ data/queries/boards/getPostDetails.sql
@@ -0,0 +1,6 @@
+SELECT
+    posts.postid as "messageuri",
+    posts.userid as "contributor",
+    posts.posteddate as "created"
+FROM
+    posts
---
+++ data/queries/serverfault/getPostDetails.sql
@@ -0,0 +1,6 @@
+SELECT
+    post.id as "messageuri",
+    post.owner_id as "contributor",
+    post.created as "created"
+FROM
+    post
